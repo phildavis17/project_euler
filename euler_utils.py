a0aeaa0760277367cac9837f68379026f6948c0e
phildavis17 <phildavis17@gmail.com>
2021-12-11 21:48:52 -0500

    initial commit

---
+++ euler_utils.py
@@ -0,0 +1,27 @@
+
+def is_prime(n: int) -> bool:
+    pass
+
+
+def nth_prime(n: int) -> int:
+    pass
+
+
+class Grid:
+    """
+    A class to make it easier to work with 2 dimensional arrays.
+    """
+
+    def __init__(self) -> None:
+        #self.height
+        #self.width
+        #self.included_points
+        #
+        pass
+
+    def get_neighbors(coord: tuple) -> list:
+        pass
+
+    def is_in_grid(coord: tuple) -> bool:
+        pass
+
